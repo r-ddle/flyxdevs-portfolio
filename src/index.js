d37172590e6c5df12409d2ce9601438c82c3b8ea
Riddle <155899302+r-ddle@users.noreply.github.com>
2024-08-13 15:45:51 +0530

    Update 1.5.1
    Fixing some deployment issues

--- src/index.js
+++ src/index.js
@@ -1,7 +1,12 @@
 import ReactDOM from 'react-dom/client';
 import "preline/preline";
 import './index.css';
-import Page from './Page.js'
+import Page from './routes/Page.js'
+import { HashRouter as Router } from "react-router-dom";
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(<Page />);
+root.render(
+    <Router>
+        <Page />
+    </Router> 
+);
