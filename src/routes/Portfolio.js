60cd2510999aca3ffa1ad83d7bbb3254106ecd97
Riddle <155899302+r-ddle@users.noreply.github.com>
2024-08-15 12:20:31 +0530

    Update 2.7
    Base Site Fully Done!
     - Base design fully completed
     - Pricing, Portfolio and About section

---
+++ src/routes/Portfolio.js
@@ -0,0 +1,78 @@
+import { motion as m } from 'framer-motion';
+import ProjectCards from './ProjectCards';
+import { Calendar } from 'lucide-react';
+
+const Portfolio = () => {
+    const megaprojects = [
+        {
+            title: "Main Project",
+            description: "Description for Project 1",
+            dateUpdated: "7 hours ago",
+            imageUrl: "https://images.unsplash.com/photo-1680868543815-b8666dba60f7?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=560&q=80"
+        },
+        {
+            title: "Project 2",
+            description: "Description for Project 2",
+            dateUpdated: "2 days ago",
+            imageUrl: "https://github.com/r-ddle/Blog-Card-Preview/raw/main/screenshot.jpg"
+        },
+    ];
+
+    const moreProjects = [
+        { title: "placeholder", date: "Jan 14, 2024", icon: "" },
+        { title: "placeholder 2", date: "Jan 14, 2024" },
+        { title: "placeholder 3", date: "Jan 14, 2024" },
+        { title: "placholder 4", date: "Jan 14, 2024" },
+        { title: "placehodler 5", date: "Jan 14, 2024" },
+    ];
+
+    return (
+        <m.div
+            initial={{ opacity: 0, y: 50 }}
+            animate={{ opacity: 1, y: 0 }}
+            transition={{ duration: 0.5 }}
+            className='relative h-fit'
+        >
+            <ProjectCards projects={megaprojects} />
+            <section className='w-full bg-three h-fit py-16 mt-10'>
+                <h3 className='text-3xl text-white font-bold text-center flex items-center justify-center gap-2'>
+                    More Projects by
+                    <img
+                        alt='Gojo'
+                        src='https://avatars.githubusercontent.com/u/157416739?s=100&v=4'
+                        className='rounded-full h-9 w-9 inline-block'
+                    />
+                    &
+                    <img
+                        alt='Riddle'
+                        src='https://avatars.githubusercontent.com/u/155899302?v=4&size=64'
+                        className='rounded-full h-9 w-9 inline-block'
+                    />
+                </h3>
+                <div className='h-fit shadow-md text-one max-w-[66rem] p-4 w-full mx-auto rounded-3xl bg-opacity-80 backdrop-filter backdrop-blur-lg my-[50px]'>
+                        <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 gap-4">
+                            {moreProjects.map((project, index) => (
+                                <div key={index} className="group flex flex-col bg-nine border border-gray-700 rounded-xl hover:bg-gray-700 transition-all duration-300">
+                                    <div className="p-4 md:p-5">
+                                        <div className="flex justify-between items-center">
+                                            <div className="mt-1 text-gray-400">
+                                                {project.icon}
+                                            </div>
+                                        </div>
+                                        <h3 className="mt-2 text-lg font-bold text-gray-300 group-hover:text-white">
+                                            {project.title}
+                                        </h3>
+                                        <p className="mt-2 text-sm text-gray-500">
+                                            {project.date}
+                                        </p>
+                                    </div>
+                                </div>
+                            ))}
+                        </div>
+                </div>
+            </section>
+        </m.div>
+    );
+};
+
+export default Portfolio;
---
+++ src/routes/ProjectCards.js
@@ -0,0 +1,82 @@
+import { useState } from 'react';
+import { ArrowBigRightDash, ArrowBigLeftDash } from 'lucide-react';
+import { motion, AnimatePresence } from 'framer-motion';
+
+const ProjectCards = ({ projects }) => {
+    const [currentIndex, setCurrentIndex] = useState(0);
+    const [direction, setDirection] = useState(0);
+
+    const nextSlide = () => {
+        setDirection(1);
+        setCurrentIndex((prevIndex) => (prevIndex + 1) % projects.length);
+    };
+
+    const prevSlide = () => {
+        setDirection(-1);
+        setCurrentIndex((prevIndex) => (prevIndex - 1 + projects.length) % projects.length);
+    };
+
+    return (
+        <div className="relative bg-white shadow-2xl shadow-six rounded-xl max-w-[66rem] mx-auto h-[600px] overflow-hidden my-16">
+            <AnimatePresence initial={false} custom={direction}>
+                <motion.div
+                    key={currentIndex}
+                    custom={direction}
+                    variants={{
+                        enter: (direction) => ({
+                            x: direction > 0 ? '100%' : '-100%',
+                            opacity: 0,
+                        }),
+                        center: {
+                            x: 0,
+                            opacity: 1,
+                        },
+                        exit: (direction) => ({
+                            x: direction < 0 ? '100%' : '-100%',
+                            opacity: 0,
+                        }),
+                    }}
+                    initial="enter"
+                    animate="center"
+                    exit="exit"
+                    transition={{ duration: 0.8 }}
+                    className="absolute inset-0 w-full h-full"
+                >
+                    <img
+                        className="w-full h-full object-cover rounded-xl"
+                        src={projects[currentIndex].imageUrl}
+                        alt={projects[currentIndex].title}
+                    />
+                    <div className="absolute top-0 left-0 right-0 p-4 md:p-5 bg-gradient-to-b from-black to-transparent text-white">
+                        <h3 className="text-xl font-extrabold text-center">
+                            {projects[currentIndex].title}
+                        </h3>
+                        <p className="mt-1 text-center">
+                            {projects[currentIndex].description}
+                        </p>
+                    </div>
+                    <div className="absolute bottom-0 left-0 right-0 p-4 md:p-5 bg-gradient-to-t from-black to-transparent">
+                        <p className="mt-5 text-xs text-white">
+                            Last Updated {projects[currentIndex].dateUpdated}
+                        </p>
+                    </div>
+                </motion.div>
+            </AnimatePresence>
+
+            <button
+                onClick={prevSlide}
+                className="absolute top-1/2 left-4 transform -translate-y-1/2 bg-white bg-opacity-50 hover:bg-opacity-75 rounded-full p-2 z-10"
+            >
+                <ArrowBigLeftDash size={24} />
+            </button>
+            <button
+                onClick={nextSlide}
+                className="absolute top-1/2 right-4 transform -translate-y-1/2 bg-white bg-opacity-50 hover:bg-opacity-75 rounded-full p-2 z-10"
+            >
+                <ArrowBigRightDash size={24} />
+            </button>
+        </div>
+    );
+};
+
+export default ProjectCards;
